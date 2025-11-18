b5feb326c1ea1d79cd3f5ae4723988f33fd1f3dc
Ycwei01200 <iixun01200.tw@gmail.com>
2025-11-18 23:45:10 +0800

    update

--- Assignment/AssignmentIV/C/hash_fn.c
+++ Assignment/AssignmentIV/C/hash_fn.c
@@ -8,6 +8,10 @@
 
    Development History:
     - 2025/11/11: Initial implementation
+    - 2025/11/17: Refactored to use hash_fn.hpp
+    - 2025/11/18: download from AssignmentIV (hash_fn.cpp/hash_fn.hpp/main.cpp)
+    - 2025/11/18: modify hash functions
+
 
    Developer: Yu-Cheng Wei <iixun01200.tw@gmail.com> 
  */
